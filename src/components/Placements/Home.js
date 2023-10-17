d4bae150a1f4cd190b72d64c7c2dad5680a297ce
Pooja Tikhe <tikhepooja11@gmail.com>
2023-10-17 20:51:15 +0530

    Understood how to use lazy loading/code chunking/code splitting/dynamic bundling/on demand loading for webapp optimization

---
+++ src/components/Placements/Home.js
@@ -0,0 +1,11 @@
+import React, { Suspense } from "react";
+
+const Home = () => {
+  return (
+    <h1 className="text-center my-5">
+      Welcome to placements facility of our application
+    </h1>
+  );
+};
+
+export default Home;
